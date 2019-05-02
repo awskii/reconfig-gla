96e6c80117a0bbc8d64e2d285d8b3f30dce7e025
awskii <artem.tsskiy@gmail.com>
2019-05-02 19:41:40 +0300

    moved message impl to separated file

---
+++ src/la/message.go
@@ -0,0 +1,47 @@
+package lattice
+
+import (
+	"bytes"
+	"encoding/gob"
+	"net"
+)
+
+type responseMessage struct {
+	Ack   bool   // false in case NACK // could be extended to uint32 to pass error code in case NACK
+	PID   uint64 // responder process ID
+	SeqNo uint64 // proposalSeqNo
+	Value Element
+}
+
+func (r *responseMessage) Bytes() []byte {
+	buf := new(bytes.Buffer)
+	enc := gob.NewEncoder(buf)
+	if err := enc.Encode(r); err != nil {
+		panic(err)
+	}
+	return buf.Bytes()
+}
+
+func (r *responseMessage) RespondToAddr() net.Addr {
+	return nil
+}
+
+type proposal struct {
+	SenderPID uint64   // proposer PID
+	SeqNo     uint64   // tx seq_no for sender account
+	Value     Element  // proposed value
+	ProcAddr  net.Addr // proposer address
+}
+
+func (p *proposal) Bytes() []byte {
+	buf := new(bytes.Buffer)
+	enc := gob.NewEncoder(buf)
+	if err := enc.Encode(p); err != nil {
+		panic(err)
+	}
+	return buf.Bytes()
+}
+
+func (p *proposal) RespondToAddr() net.Addr {
+	return p.ProcAddr
+}
